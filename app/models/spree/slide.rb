e7d7498319b4e96145fd6a30b0cf2776751e6d63
mrbrdo <mrbrdo@gmail.com>
2024-09-27 22:11:17 +0200

    Allow blank slide name

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -14,7 +14,7 @@ class Spree::Slide < ActiveRecord::Base
   has_one_attached :image
 
   validates :link_url, presence: true, url: true, unless: -> { product }
-  validates :name, :image, presence: true, unless: -> { product }
+  validates :image, presence: true, unless: -> { product }
   validates :image, content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
 
   scope :published, -> { where(published: true).order('position ASC') }
